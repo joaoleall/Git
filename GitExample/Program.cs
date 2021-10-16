f596f532361c34aa14dce7c9e0c05a7149182af7
João Leal <joaopedrocasca2@gmail.com>
2021-10-16 17:25:27 +0100

    Acrescentada mensagem de bom dia!

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -8,6 +8,7 @@ namespace GitExample
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está?");
             Console.ReadLine();
         }
     }
